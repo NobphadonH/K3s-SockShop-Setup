ef38b2e552adf4aa5b6de49f2fe3f971d888a31a
Nobphadon Hankittikanjana <nopz3456z@gmail.com>
2026-01-10 15:54:22 +0700

    fix trigger_locust.sh

--- experiment_ec2/trigger_locust.sh
+++ experiment_ec2/trigger_locust.sh
@@ -1,5 +1,7 @@
-kubectl --kubeconfig ~/k3s.yaml run -n locust locust-trigger --rm -i --restart=Never \
+kubectl --kubeconfig ~/k3s.yaml run -n locust locust-trigger \
+  --rm -i --restart=Never \
+  --overrides='{"metadata":{"annotations":{"sidecar.istio.io/inject":"false"}}}' \
   --image=curlimages/curl:8.5.0 -- \
-  curl -X POST "http://locust-ui.sock-shop.svc.cluster.local:8089/swarm" \
+  curl -sS -X POST "http://locust-ui:8089/swarm" \
   -d "user_count=50" \
   -d "spawn_rate=5"
