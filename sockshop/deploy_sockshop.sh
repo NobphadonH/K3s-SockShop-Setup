ca0a62309eb3a3b08f84e12bff6711be28b20486
Nobphadon Hankittikanjana <nopz3456z@gmail.com>
2026-03-03 14:41:23 +0700

    fix session-db redis bug

--- sockshop/deploy_sockshop.sh
+++ sockshop/deploy_sockshop.sh
@@ -7,6 +7,12 @@ kubectl label ns "$NAMESPACE" istio-injection=enabled --overwrite
 
 kubectl apply -n sock-shop -f https://raw.githubusercontent.com/microservices-demo/microservices-demo/master/deploy/kubernetes/complete-demo.yaml
 
+# Wait for session-db and patch the read-only filesystem bug
+until kubectl -n sock-shop get deploy session-db >/dev/null 2>&1; do
+  sleep 1
+done
+kubectl patch deployment session-db -n sock-shop -p '{"spec":{"template":{"spec":{"volumes":[{"name":"data-dir","emptyDir":{}}],"containers":[{"name":"session-db","volumeMounts":[{"name":"data-dir","mountPath":"/data"}]}]}}}}'
+
 until kubectl -n sock-shop get deploy carts-db >/dev/null 2>&1; do
   sleep 1
 done
